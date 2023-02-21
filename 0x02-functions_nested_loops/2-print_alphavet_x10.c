cbafb21bafe5e01b538cf729f7cd9e4dc19e3cbe
Yonas Getahun <yonasge57@gmail.com>
2023-02-21 09:21:58 -0800

    j

---
+++ 0x02-functions_nested_loops/2-print_alphavet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing the code
+ *
+ * Return: Always success
+*/
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		print_alphabet();
+	}
+}
