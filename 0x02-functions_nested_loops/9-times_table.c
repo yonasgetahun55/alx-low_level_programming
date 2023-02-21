cfe18fedfa5cd100c2e6c481cb7235247bcfd56a
Yonas Getahun <yonasge57@gmail.com>
2023-02-21 12:48:27 -0800

    j

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,8 @@ void times_table(void)
 		for (col = 0; col <= 9; col++)
 		{
 			result = row * col;
-			_putchar(result + '0');
+			_putchar((result/10) + '0');
+			_putchar((result%10) + '0');
 			if (col == 9)
 				continue;
 			else
