a6604c721abea4dd819bb6e14e1a559e531d4984
Yonas Getahun <yonasge57@gmail.com>
2023-02-21 08:54:57 -0800

    j

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_alphabet - displays the alpahbets
+ *
+ * Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	_putchar('a');
+	_putchar('b');
+	_putchar('c');
+	_putchar('d');
+	_putchar('e');
+	_putchar('f');
+	_putchar('g');
+	_putchar('h');
+	_putchar('i');
+	_putchar('j');
+	_putchar('k');
+	_putchar('l');
+	_putchar('m');
+	_putchar('n');
+	_putchar('o');
+	_putchar('p');
+	_putchar('q');
+	_putchar('r');
+	_putchar('s');
+	_putchar('t');
+	_putchar('u');
+	_putchar('v');
+	_putchar('w');
+	_putchar('x');
+	_putchar('y');
+	_putchar('z');
+	_putchar('\n');
+	return (0);
+}
