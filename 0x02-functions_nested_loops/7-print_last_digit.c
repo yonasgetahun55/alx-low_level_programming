2c25e72f57bcf8ecd329e849ee448e414488cd2a
Yonas Getahun <yonasge57@gmail.com>
2023-02-21 10:45:01 -0800

    h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - function
+ * @n: the digit
+ *
+ * Return: returns last digit
+ */
+int print_last_digit(int n)
+{
+	int y;
+
+	y = n % 10;
+	return (y);
+}
