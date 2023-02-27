5fa5c9e9aae141db89a105d5efa30d4b5fd89fec
Yonas Getahun <yonasge57@gmail.com>
2023-02-27 10:42:15 -0800

    h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - function
+ * @s: parmameter
+ *
+ * Return: f
+ */
+void print_rev(char *s)
+{
+	printf("%s", strrev(s));
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rev_string - function
+ * @s: parmaeters
+ *
+ * Return: jvoid
+ */
+void rev_string(char *s)
+{
+	
+}
