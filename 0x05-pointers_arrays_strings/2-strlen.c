37bcf9594aa2387cc09b2c620a46b88bfcb49271
Yonas Getahun <yonasge57@gmail.com>
2023-02-27 10:11:47 -0800

    h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function
+ * @s: parameter
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
