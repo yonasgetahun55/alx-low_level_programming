c09764707e5be1170fad01384ba9f60cbaa2a92b
Yonas Getahun <yonasge57@gmail.com>
2023-03-01 08:33:11 -0800

    h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function
+ * @dest: param one
+ * @src: param sec
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
