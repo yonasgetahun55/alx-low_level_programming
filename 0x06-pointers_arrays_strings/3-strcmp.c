35a0cf6133a7e43064db8f676b3a11d5ad09161d
Yonas Getahun <yonasge57@gmail.com>
2023-03-01 09:04:04 -0800

    h

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function
+ * @s1: first param
+ * @s2: second param
+ *
+ * Return: pointer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
