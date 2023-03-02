7d98577e5990ebeccb827899b8f0eb42ada3ad9c
Yonas Getahun <yonasge57@gmail.com>
2023-03-01 20:50:42 -0800

    h

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,9 @@ char *string_toupper(char *c)
 {
 	int i;
 
-	for (i = 0; c[i] != '\n'; i++)
+	for (i = 0; c[i] != '\0'; i++)i
 	{
-		if (c[i] >= 97 && c[i] <= 122)
+		if (c[i] >= 'a' && c[i] <= 'z')
 			c[i] = c[i] - 32;
 	}
 
